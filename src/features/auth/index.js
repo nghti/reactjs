b8f991a4bfe421bbd966b515ee1902a62d90d154
Tu.Trinh <tu.th@neo-lab.vn>
2019-10-28 17:21:08 +0700

    up login

--- src/features/auth/index.js
+++ src/features/auth/index.js
@@ -1,6 +1,6 @@
 import authReducer from "./store/reducer";
 import { authSaga } from "./store/saga";
-import PageLogin from "./Login_ex";
+import PageLogin from "./Login_ex1";
 
 export {
   authReducer,
